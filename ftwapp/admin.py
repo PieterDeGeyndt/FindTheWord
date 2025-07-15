4ee0c299bfca35302c38205cc9e8ef9fe38e3cc7
PieterDeGeyndt <pieter@test2impress.be>
2025-07-16 01:37:07 +0200

    Rearranged admin.py

--- ftwapp/admin.py
+++ ftwapp/admin.py
@@ -6,7 +6,6 @@ from .models import Category, Subcategory, Word
 from django.urls import reverse
 from django.utils.html import format_html
 
-
 # -------- Category --------
 class CategoryResource(resources.ModelResource):
     class Meta:
@@ -32,14 +31,22 @@ class CategoryAdminForm(forms.ModelForm):
 @admin.register(Category)
 class CategoryAdmin(ImportExportModelAdmin):
     form = CategoryAdminForm
+    resource_class = CategoryResource
+
+    readonly_fields = ('image_preview',)
+    fields = ('image_preview', 'name', 'parent', 'image', 'image_search')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
+        return "(No image)"
+    image_preview.short_description = "Image Preview"
 
     class Media:
         js = (
             'https://code.jquery.com/jquery-3.6.0.min.js',
             'ftwapp/image_search.js',
         )
-
-    resource_class = CategoryResource
 
 
 # -------- Subcategory --------
@@ -67,6 +74,16 @@ class SubcategoryAdminForm(forms.ModelForm):
 @admin.register(Subcategory)
 class SubcategoryAdmin(ImportExportModelAdmin):
     form = SubcategoryAdminForm
+    resource_class = SubcategoryResource
+
+    readonly_fields = ('image_preview',)
+    fields = ('image_preview', 'name', 'category', 'image', 'image_search')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
+        return "(No image)"
+    image_preview.short_description = "Image Preview"
 
     class Media:
         js = (
@@ -74,14 +91,12 @@ class SubcategoryAdmin(ImportExportModelAdmin):
             'ftwapp/image_search.js',
         )
 
-    resource_class = SubcategoryResource
-
 
 # -------- Word --------
 class WordResource(resources.ModelResource):
     class Meta:
         model = Word
-        fields = ('id', 'name', 'subcategory', 'image',)
+        fields = '__all__'
 
 class WordAdminForm(forms.ModelForm):
     image_search = forms.CharField(
@@ -104,49 +119,42 @@ class WordAdmin(ImportExportModelAdmin):
     form = WordAdminForm
     resource_class = WordResource
 
-    class Media:
-        js = (
-            'https://code.jquery.com/jquery-3.6.0.min.js',
-            'ftwapp/image_search.js',
-        )
+    readonly_fields = ('image_preview',)
+    fields = ('image_preview', 'image', 'image_search','name', 'subcategory', 'description')
 
     change_form_template = "admin/ftwapp/word/change_form.html"
-    
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
+        return "(No image)"
+    image_preview.short_description = "Image Preview"
+
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         extra_context = extra_context or {}
 
         if object_id:
             words = list(Word.objects.order_by('id').values_list('id', flat=True))
-            print("All word IDs:", words)
-            print("Current object_id:", object_id)
-
             try:
                 current_index = words.index(int(object_id))
             except ValueError:
                 current_index = -1
-            print("Current index:", current_index)
 
             if current_index != -1:
                 if current_index > 0:
                     prev_id = words[current_index - 1]
                     prev_word_url = reverse("admin:ftwapp_word_change", args=[prev_id])
                     extra_context['prev_word_url'] = prev_word_url
-                    print("Prev URL:", prev_word_url)
 
                 if current_index < len(words) - 1:
                     next_id = words[current_index + 1]
                     next_word_url = reverse("admin:ftwapp_word_change", args=[next_id])
                     extra_context['next_word_url'] = next_word_url
-                    print("Next URL:", next_word_url)
 
-        print("Extra context keys:", extra_context.keys())
         return super().changeform_view(request, object_id, form_url, extra_context)
 
-    readonly_fields = ('image_preview',)
-
-    def image_preview(self, obj):
-        if obj.image:
-            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
-        return "(No image)"
-
-    image_preview.short_description = "Image Preview"
+    class Media:
+        js = (
+            'https://code.jquery.com/jquery-3.6.0.min.js',
+            'ftwapp/image_search.js',
+        )
